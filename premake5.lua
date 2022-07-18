90f0e3276d2f7158d3930529c9ad3d41299ec56e
GodOfPro <1387ilia@gmail.com>
2022-07-18 19:22:24 +0430

    Added premake

--- premake5.lua
+++ premake5.lua
@@ -1,55 +1,105 @@
 workspace "Rexal"
-	architecure "x64"
-	
+	architecture "x64"
+
 	configurations
 	{
 		"Debug",
 		"Release",
 		"Dist"
 	}
-	
-outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecure}"
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
 project "Rexal"
 	location "Rexal"
 	kind "SharedLib"
-	language"C++"
-	
+	language "C++"
+
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	targetdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-	
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
 	files
 	{
-		"{prj.name}/src/**.h",
-		"{prj.name}/src/**.cpp"
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
 	}
-	
-	include
+
+	includedirs
 	{
-		"{prj.name}/vendor/spdlog/include"
+		"%{prj.name}/vendor/spdlog/include"
 	}
-	
+
 	filter "system:windows"
 		cppdialect "C++17"
 		staticruntime "On"
 		systemversion "latest"
-		
+
 		defines
 		{
 			"RX_PLATFORM_WINDOWS",
 			"RX_BUILD_DLL"
 		}
-		
+
 		postbuildcommands
 		{
-			("{COPY} %{cfg.buildtarger.relpath} ../bin/" .. outputdir .. "/Sandbox)
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
 		}
-	
+
 	filter "configurations:Debug"
 		defines "RX_DEBUG"
 		symbols "On"
+
 	filter "configurations:Release"
 		defines "RX_RELEASE"
 		optimize "On"
+
+	filter "configurations:Dist"
+		defines "RX_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Rexal/vendor/spdlog/include",
+		"Rexal/src"
+	}
+
+	links
+	{
+		"Rexal"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"RX_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "RX_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "RX_RELEASE"
+		optimize "On"
+
 	filter "configurations:Dist"
 		defines "RX_DIST"
 		optimize "On"
