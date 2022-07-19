970e8ed32c530982441f2e52cbc8293868feb814
GodOfPro <1387ilia@gmail.com>
2022-07-19 20:17:06 +0430

    Fixed wrong spdlog api calls

--- Rexal/src/Rexal/Log.h
+++ Rexal/src/Rexal/Log.h
@@ -23,11 +23,11 @@ namespace Rexal {
 #define RX_CORE_INFO(...)     ::Rexal::Log::GetCoreLogger()->info(__VA_ARGS__)
 #define RX_CORE_WARN(...)     ::Rexal::Log::GetCoreLogger()->warn(__VA_ARGS__)
 #define RX_CORE_ERROR(...)    ::Rexal::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define RX_CORE_FATAL(...)    ::Rexal::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+#define RX_CORE_CRITICAL(...)    ::Rexal::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
 // Client log macros
 #define RX_TRACE(...)         ::Rexal::Log::GetClientLogger()->trace(__VA_ARGS__)
 #define RX_INFO(...)          ::Rexal::Log::GetClientLogger()->info(__VA_ARGS__)
 #define RX_WARN(...)          ::Rexal::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define RX_ERROR(...)         ::Rexal::Log::GetClientLogger()->error(__VA_ARGS__)
-#define RX_FATAL(...)         ::Rexal::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#define RX_CRITICAL(...)         ::Rexal::Log::GetClientLogger()->critical(__VA_ARGS__)
