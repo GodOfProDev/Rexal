b468bb494f8cfa9dc916dd7632a307e6ff7395b0
GodOfPro <1387ilia@gmail.com>
2022-07-20 18:55:05 +0430

    Added glm

--- premake5.lua
+++ premake5.lua
@@ -16,6 +16,7 @@ IncludeDir = {}
 IncludeDir["GLFW"] = "Rexal/vendor/GLFW/include"
 IncludeDir["Glad"] = "Rexal/vendor/Glad/include"
 IncludeDir["ImGui"] = "Rexal/vendor/imgui"
+IncludeDir["glm"] = "Rexal/vendor/glm"
 
 group "Dependencies"
 	include "Rexal/vendor/GLFW"
@@ -38,7 +39,9 @@ project "Rexal"
 	files
 	{
 		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/vendor/glm/glm/**.hpp",
+		"%{prj.name}/vendor/glm/glm/**.inl"
 	}
 
 	includedirs
@@ -47,7 +50,8 @@ project "Rexal"
 		"%{prj.name}/vendor/spdlog/include",
 		"%{IncludeDir.GLFW}",
 		"%{IncludeDir.Glad}",
-		"%{IncludeDir.ImGui}"
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.glm}"
 	}
 	
 	links
@@ -107,7 +111,8 @@ project "Sandbox"
 	includedirs
 	{
 		"Rexal/vendor/spdlog/include",
-		"Rexal/src"
+		"Rexal/src",
+		"%{IncludeDir.glm}"
 	}
 
 	links
