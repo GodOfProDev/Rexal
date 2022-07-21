3da2d68b84a94eed944a5a18fd9c7aa874ea39bd
GodOfPro <1387ilia@gmail.com>
2022-07-21 17:37:43 +0430

    Added a triangle

--- Rexal/src/Platform/OpenGL/OpenGLContext.cpp
+++ Rexal/src/Platform/OpenGL/OpenGLContext.cpp
@@ -15,6 +15,12 @@ namespace Rexal {
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		RX_CORE_ASSERT(status, "Failed to initialize Glad");
+
+		RX_CORE_INFO("OpenGL Info:");
+
+		RX_CORE_INFO("	Vendor: {0}", glGetString(GL_VENDOR));
+		RX_CORE_INFO("	Renderer: {0}", glGetString(GL_RENDERER));
+		RX_CORE_INFO("	Version: {0}", glGetString(GL_VERSION));
 	}
 	void OpenGLContext::SwapBuffers()
 	{
