8d7a2b3eb6b1fff003f9bada6b0617ad8952ee07
GodOfPro <1387ilia@gmail.com>
2022-08-10 17:15:17 +0430

    Added Shader Abstraction To OpenGLShader

--- Rexal/src/Rexal/Renderer/Renderer.cpp
+++ Rexal/src/Rexal/Renderer/Renderer.cpp
@@ -2,6 +2,7 @@
 #include "Renderer.h"
 
 #include "OrthographicCamera.h"
+#include "Platform/OpenGL/OpenGLShader.h"
 
 namespace Rexal {
 
@@ -19,8 +20,8 @@ namespace Rexal {
 	void Renderer::Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray, const glm::mat4& transform)
 	{
 		shader->Bind();
-		shader->UploadUniformMat4("u_ViewProjection", m_SceneData->ViewProjectionMatrix);
-		shader->UploadUniformMat4("u_Transform", transform);
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_ViewProjection", m_SceneData->ViewProjectionMatrix);
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_Transform", transform);
 
 		vertexArray->Bind();
 		RenderCommand::DrawIndexed(vertexArray);
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,12 +1,16 @@
 #include <Rexal.h>
 
+#include <imgui/imgui.h>
+#include "Platform/OpenGL/OpenGLShader.h"
+
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class ExampleLayer : public Rexal::Layer
 {
 public:
 	ExampleLayer()
-		: Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.0f), m_SquarePosition(0.0f)
+		: Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.0f)
 	{
 		m_VertexArray.reset(Rexal::VertexArray::Create());
 		m_SquareVA.reset(Rexal::VertexArray::Create());
@@ -88,8 +92,8 @@ public:
 		)";
 		#pragma endregion
 
-		#pragma region BlueShader
-		std::string blueShaderVertexSrc = R"(
+		#pragma region FlatColorShader
+		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
@@ -98,6 +102,7 @@ public:
 			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
+
 			void main()
 			{
 				v_Position = a_Position;
@@ -105,20 +110,24 @@ public:
 			}
 		)";
 
-		std::string blueShaderFragmentSrc = R"(
+		std::string flatColorShaderFragmentSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) out vec4 color;
+
 			in vec3 v_Position;
+
+			uniform vec3 u_Color;
+
 			void main()
 			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
+				color = vec4(u_Color, 1.0);
 			}
 		)";
 		#pragma endregion
 
-		m_Shader.reset(new Rexal::Shader(vertexSrc, fragmentSrc));
-		m_BlueShader.reset(new Rexal::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
+		m_Shader.reset(Rexal::Shader::Create(vertexSrc, fragmentSrc));
+		m_FlatColorShader.reset(Rexal::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 	}
 
 	void OnUpdate(Rexal::Timestep ts) override
@@ -150,19 +159,30 @@ public:
 
 		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 
+		std::dynamic_pointer_cast<Rexal::OpenGLShader>(m_FlatColorShader)->Bind();
+		std::dynamic_pointer_cast<Rexal::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
+
+
 		for (int y = 0; y < 10; y++)
 		{
 			for (int x = 0; x < 10; x++)
 			{
 				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
 				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
-				Rexal::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+				Rexal::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
 			}
 		}
 
 		Rexal::Renderer::Submit(m_Shader, m_VertexArray);
 
 		Rexal::Renderer::EndScene();
+	}
+
+	virtual void OnImGuiRender() override
+	{
+		ImGui::Begin("Settings");
+		ImGui::ColorEdit3("Square Color", glm::value_ptr(m_SquareColor));
+		ImGui::End();
 	}
 
 	void OnEvent(Rexal::Event& event) override
@@ -173,7 +193,7 @@ private:
 	std::shared_ptr<Rexal::Shader> m_Shader;
 	std::shared_ptr<Rexal::VertexArray> m_VertexArray;
 
-	std::shared_ptr<Rexal::Shader> m_BlueShader;
+	std::shared_ptr<Rexal::Shader> m_FlatColorShader;
 	std::shared_ptr<Rexal::VertexArray> m_SquareVA;
 
 	Rexal::OrthographicCamera m_Camera;
@@ -183,7 +203,7 @@ private:
 	float m_CameraRotation = 0.0f;
 	float m_CameraRotationSpeed = 180.0f;
 
-	glm::vec3 m_SquarePosition;
+	glm::vec3 m_SquareColor = {0.2f, 0.3f, 0.8f};
 };
 
 class Sandbox : public Rexal::Application
