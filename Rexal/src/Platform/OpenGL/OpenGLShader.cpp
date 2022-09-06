c3f179114ae774a05f5ffb09543fff4f82af22d4
GodOfPro <1387ilia@gmail.com>
2022-09-06 13:59:05 +0430

    Added file reading check

--- Rexal/src/Platform/OpenGL/OpenGLShader.cpp
+++ Rexal/src/Platform/OpenGL/OpenGLShader.cpp
@@ -60,10 +60,18 @@ namespace Rexal {
 		if (in)
 		{
 			in.seekg(0, std::ios::end);
-			result.resize(in.tellg());
-			in.seekg(0, std::ios::beg);
-			in.read(&result[0], result.size());
-			in.close();
+			size_t size = in.tellg();
+			if (size != -1)
+			{
+				result.resize(size);
+				in.seekg(0, std::ios::beg);
+				in.read(&result[0], size);
+				in.close();
+			}
+			else
+			{
+				RX_CORE_ERROR("Could not open file {0}", filepath);
+			}
 		}
 		else
 		{
