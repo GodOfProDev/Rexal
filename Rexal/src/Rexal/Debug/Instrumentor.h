98a049a0e6ee34c09fe3d7d663fee35b9897a42c
GodOfPro <1387ilia@gmail.com>
2022-09-06 14:14:00 +0430

    Auto deducing an availabe __FUNCSIG__ definition

--- Rexal/src/Rexal/Debug/Instrumentor.h
+++ Rexal/src/Rexal/Debug/Instrumentor.h
@@ -124,10 +124,31 @@ namespace Rexal {
 
 #define RX_PROFILE 0
 #if RX_PROFILE
-#define RX_PROFILE_BEGIN_SESSION(name, filepath) ::Rexal::Instrumentor::Get().BeginSession(name, filepath)
-#define RX_PROFILE_END_SESSION() ::Rexal::Instrumentor::Get().EndSession()
-#define RX_PROFILE_SCOPE(name) ::Rexal::InstrumentationTimer timer##__LINE__(name);
-#define RX_PROFILE_FUNCTION() RX_PROFILE_SCOPE(__FUNCSIG__) 
+    // Resolve which function signature macro will be used. Note that this only
+    // is resolved when the (pre)compiler starts, so the syntax highlighting
+    // could mark the wrong one in your editor!
+    #if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
+    #define RX_FUNC_SIG __PRETTY_FUNCTION__
+    #elif defined(__DMC__) && (__DMC__ >= 0x810)
+    #define RX_FUNC_SIG __PRETTY_FUNCTION__
+    #elif (defined(__FUNCSIG__) || (_MSC_VER))
+    #define RX_FUNC_SIG __FUNCSIG__
+    #elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
+    #define RX_FUNC_SIG __FUNCTION__
+    #elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
+    #define RX_FUNC_SIG __FUNC__
+    #elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+    #define RX_FUNC_SIG __func__
+    #elif defined(__cplusplus) && (__cplusplus >= 201103)
+    #define RX_FUNC_SIG __func__
+    #else
+    #define RX_FUNC_SIG "RX_FUNC_SIG unknown!"
+    #endif
+
+    #define RX_PROFILE_BEGIN_SESSION(name, filepath) ::Rexal::Instrumentor::Get().BeginSession(name, filepath)
+    #define RX_PROFILE_END_SESSION() ::Rexal::Instrumentor::Get().EndSession()
+    #define RX_PROFILE_SCOPE(name) ::Rexal::InstrumentationTimer timer##__LINE__(name);
+    #define RX_PROFILE_FUNCTION() RX_PROFILE_SCOPE(RX_FUNC_SIG) 
 #else
 #define RX_PROFILE_BEGIN_SESSION(name, filepath)
 #define RX_PROFILE_END_SESSION()
