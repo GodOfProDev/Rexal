c910cbf68b964850487925ba05e34cb281d7d7b1
GodOfPro <1387ilia@gmail.com>
2022-11-15 16:25:20 +0330

    virtual destructors

--- Rexal/src/Rexal/Events/Event.h
+++ Rexal/src/Rexal/Events/Event.h
@@ -38,6 +38,8 @@ namespace Rexal {
 	class Event
 	{
 	public:
+		virtual ~Event() = default;
+
 		bool Handled = false;
 
 		virtual EventType GetEventType() const = 0;
--- Rexal/src/Rexal/Renderer/GraphicsContext.h
+++ Rexal/src/Rexal/Renderer/GraphicsContext.h
@@ -4,6 +4,8 @@ namespace Rexal {
 	class GraphicsContext
 	{
 	public:
+		virtual ~GraphicsContext() = default;
+
 		virtual void Init() = 0;
 		virtual void SwapBuffers() = 0;
 
