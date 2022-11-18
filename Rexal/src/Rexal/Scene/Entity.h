147e93971a8177abc0c85136c2df6eb7222d5f1a
GodOfPro <1387ilia@gmail.com>
2022-11-18 14:36:40 +0330

    GetName entity

--- Rexal/src/Rexal/Scene/Entity.h
+++ Rexal/src/Rexal/Scene/Entity.h
@@ -44,6 +44,8 @@ namespace Rexal {
 		operator entt::entity() const { return m_EntityHandle; }
 		operator uint32_t() const { return (uint32_t)m_EntityHandle; }
 
+		const std::string& GetName() { return GetComponent<TagComponent>().Tag; }
+
 		bool operator==(const Entity& other) const
 		{
 			return m_EntityHandle == other.m_EntityHandle && m_Scene == other.m_Scene;
