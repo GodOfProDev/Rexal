762756fd026d2656640a47b076c66b1fc62dacf1
GodOfPro <1387ilia@gmail.com>
2023-10-26 23:50:20 +0330

    Add Missing SetFloat2 in Shader

--- Rexal/src/Platform/OpenGL/OpenGLShader.cpp
+++ Rexal/src/Platform/OpenGL/OpenGLShader.cpp
@@ -217,6 +217,13 @@ namespace Rexal {
 		UploadUniformFloat(name, value);
 	}
 
+	void OpenGLShader::SetFloat2(const std::string& name, const glm::vec2& value)
+	{
+		RX_PROFILE_FUNCTION();
+
+		UploadUniformFloat2(name, value);
+	}
+
 	void OpenGLShader::SetFloat3(const std::string& name, const glm::vec3& value)
 	{
 		RX_PROFILE_FUNCTION();
