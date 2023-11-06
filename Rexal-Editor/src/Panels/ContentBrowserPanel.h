9a3b7c49f66dbb9ec584e214c4366cf01ef6df32
GodOfPro <1387ilia@gmail.com>
2023-11-06 18:00:35 +0330

    Show the icons in the content browser panel

--- Rexal-Editor/src/Panels/ContentBrowserPanel.h
+++ Rexal-Editor/src/Panels/ContentBrowserPanel.h
@@ -2,6 +2,8 @@
 
 #include <filesystem>
 
+#include "Rexal/Renderer/Texture.h"
+
 namespace Rexal {
 
 	class ContentBrowserPanel
@@ -12,6 +14,9 @@ namespace Rexal {
 		void OnImGuiRender();
 	private:
 		std::filesystem::path m_CurrentDirectory;
+
+		Ref<Texture2D> m_DirectoryIcon;
+		Ref<Texture2D> m_FileIcon;
 	};
 
 }
