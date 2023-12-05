4be80825be53e41902b41550636b4e8855de17eb
GodOfPro <1387ilia@gmail.com>
2023-12-05 21:18:12 +0330

    Call start/stop when going into/out of edit mode

--- Rexal-Editor/src/EditorLayer.cpp
+++ Rexal-Editor/src/EditorLayer.cpp
@@ -514,11 +514,12 @@ namespace Rexal {
 	void EditorLayer::OnScenePlay()
 	{
 		m_SceneState = SceneState::Play;
+		m_ActiveScene->OnRuntimeStart();
 	}
 
 	void EditorLayer::OnSceneStop()
 	{
 		m_SceneState = SceneState::Edit;
-
+		m_ActiveScene->OnRuntimeStop();
 	}
 }
