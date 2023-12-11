f76150caff9c0c508d44815e8115336a776c1a88
GodOfPro <1387ilia@gmail.com>
2023-12-11 21:41:21 +0330

    Add CircleRendererComponent to the list
    Add CircleRendererComponent to the list of available components

--- Rexal-Editor/src/Panels/SceneHierarchyPanel.cpp
+++ Rexal-Editor/src/Panels/SceneHierarchyPanel.cpp
@@ -269,6 +269,7 @@ namespace Rexal {
 		{
 			DisplayAddComponentEntry<CameraComponent>("Camera");
 			DisplayAddComponentEntry<SpriteRendererComponent>("Sprite Renderer");
+			DisplayAddComponentEntry<CircleRendererComponent>("Circle Renderer");
 			DisplayAddComponentEntry<Rigidbody2DComponent>("Rigidbody 2D");
 			DisplayAddComponentEntry<BoxCollider2DComponent>("Box Collider 2D");
 
@@ -390,6 +391,13 @@ namespace Rexal {
 			}
 
 			ImGui::DragFloat("Tiling Factor", &component.TilingFactor, 0.1f, 0.0f, 100.0f);
+		});
+
+		DrawComponent<CircleRendererComponent>("Circle Renderer", entity, true, [](auto& component)
+		{
+			ImGui::ColorEdit4("Color", glm::value_ptr(component.Color));
+			ImGui::DragFloat("Thickness", &component.Thickness, 0.025f, 0.0f, 1.0f);
+			ImGui::DragFloat("Fade", &component.Fade, 0.00025f, 0.0f, 1.0f);
 		});
 
 		DrawComponent<Rigidbody2DComponent>("Rigidbody 2D", entity, true, [](auto& component)
